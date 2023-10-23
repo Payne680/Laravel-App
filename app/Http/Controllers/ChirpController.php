3286c42eea949ed0672c14abd27a7a80b3678e85
PENN JR <pennjunior680@gmail.com>
2023-10-23 17:08:04 +0100

    Adding a chirp route and a chirp form to my project

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,17 +2,23 @@
 
 namespace App\Http\Controllers;
 
+// use Illuminate\Http\Response;
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
-        //
+        // return response('Hello, World');
+        return Inertia::render('Chirps/Index', [
+            //
+        ]);
     }
 
     /**
